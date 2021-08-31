4d55508cd6b733353aa69e096bf95e42a4d03690
neom200 <lucas@lucaslenoch.com.br>
2021-08-31 02:03:50 +0000

    Adicionado primeira rota basica

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 8080
+const path = require('path')
+
+app.use(cors());
+app.use(express.static('public'));
 
 app.get("/", function(req,res) {
     console.log(`Server running on port ${PORT}`);
     res.status(200);
-    res.send("Opa");
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.end()
 });
 
 app.listen(PORT);

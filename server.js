40b8ff07258447e26006dfa7fc3fc93c1220105b
neom200 <lucas@lucaslenoch.com.br>
2021-09-01 00:10:30 +0000

    começo do site da Família do Pesadelo

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const adm = require('./rotas');
 const path = require('path');
 
 app.use(cors());
 app.use(express.static('public'));
 
-app.use('/adm', adm);
 app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/pages/index.html'));
 })
 
 const PORT = 8080
